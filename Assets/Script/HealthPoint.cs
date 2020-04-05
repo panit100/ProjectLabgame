73a4816c5461053b11c8684010558b8daa7efeb6
BlackHold2K <panit.cuteza@mail.kmutt.ac.th>
2020-04-05 23:41:08 +0700

    "HP finish but dont have Image"

---
+++ Assets/Script/HealthPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthPoint : MonoBehaviour
+{
+
+    private TextMeshProUGUI HealthPointText;
+    public Player player;
+
+
+    void Start()
+    {
+        HealthPointText = GetComponent<TextMeshProUGUI>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        HealthPointText.text = "HP : " + player.CurrentHealth;
+    }
+
+
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(CurrentHealth <= 0){
+                CurrentHealth = 0;
+            }else TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int Damage){
+        CurrentHealth -= Damage;
+    }
+}
