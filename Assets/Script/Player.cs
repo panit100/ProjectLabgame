5f1202c2e8d4206cf46d74888dd185368a75a140
BlackHold2K <panit.cuteza@mail.kmutt.ac.th>
2020-04-14 14:47:19 +0700

    HealthBar and Script

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,20 +8,20 @@ public class Player : MonoBehaviour
     public int MaxHealth = 100;
     public int CurrentHealth;
 
+    public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         CurrentHealth = MaxHealth;
+        healthBar.SetMaxHealth(MaxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)){
-            if(CurrentHealth <= 0){
-                CurrentHealth = 0;
-            }else TakeDamage(10);
+            TakeDamage(10);
         }
     }
 
@@ -29,5 +29,7 @@ public class Player : MonoBehaviour
     //May have to do in another script
     public void TakeDamage(int Damage){
         CurrentHealth -= Damage;
+
+        healthBar.SetHealth(CurrentHealth);
     }
 }
