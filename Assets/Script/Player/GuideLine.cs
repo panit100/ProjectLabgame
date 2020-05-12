d6332daea416abb3b3fe13fc1ebaf6d6f3cf4580
BlackHold2K <panit.cuteza@mail.kmutt.ac.th>
2020-05-12 19:32:10 +0700

    Test commit

---
+++ Assets/Script/Player/GuideLine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideLine : MonoBehaviour
+{
+    Ray shootRay;
+    LineRenderer gunLine;
+    public PlayerShooting playerShooting;
+
+    void Awake()
+    {
+        gunLine = GetComponent<LineRenderer>();        
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GuideLinePerk();
+    }
+
+    void GuideLinePerk(){
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+        
+        gunLine.SetPosition(0,transform.position);
+        
+        gunLine.SetPosition(1,shootRay.origin+shootRay.direction * playerShooting.weapons[playerShooting.currentWeapon].range);
+
+    }
+}
