c27bd327c48a7214fc670defb25aa3d6fffe8d98
BlackHold2K <panit.cuteza@mail.kmutt.ac.th>
2020-05-13 15:17:01 +0700

    Pause

--- Assets/Script/Pause/PauseMenu.cs
+++ Assets/Script/Pause/PauseMenu.cs
@@ -42,7 +42,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu() 
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu"); //Add Menu Scenes
+        SceneManager.LoadScene("Shop"); //Add Menu Scenes
     }
     public void QuitGame()
     {
